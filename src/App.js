f04673d6d72f2cdab70e57eda1ebea68490288f6
Valentin <qweqweq@qwe>
2020-11-24 20:42:02 +0300

    Капча

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
 import React, {Component} from 'react';
 import './App.css';
 import Navbar from './components/Navbar/Navbar';
-import {BrowserRouter, Route, withRouter} from "react-router-dom";
+import {HashRouter, Route, withRouter} from "react-router-dom";
 
-import UsersContainer from "./components/User/UsersContainer";
+import UsersContainer from "./components/Users/UsersContainer";
 import HeaderContainer from "./components/Header/HeaderContainer";
 import LoginPage from "./components/Login/Login";
 import {connect, Provider} from "react-redux";
@@ -59,11 +59,11 @@ let AppContainer = compose(
     connect(mapStateToProps, {initializeApp}))(App);
 
 const SamuraiJSApp = (props) => {
-   return <BrowserRouter>
+   return <HashRouter >
         <Provider store={store}>
             <AppContainer />
         </Provider>
-    </BrowserRouter>
+    </HashRouter>
 }
 
 export default SamuraiJSApp;
